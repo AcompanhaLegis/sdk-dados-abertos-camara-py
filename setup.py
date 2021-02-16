2dece44af6dcb4cddb219a1e5e952c10f7de885b
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2021-02-16 21:50:04 +0100

    Add requests to deps on setup

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='sdk-dados-abertos-camara',
-    version='0.0.6',
+    version='0.0.7',
     author='AcompanhaLegis',
     description='SDK to use Dados Abertos da Camara dos Deputados',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/acompanhalegis/sdk-dados-abertos-camara-py',
     packages=setuptools.find_packages(),
+    install_requires=['requests==2.25.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
